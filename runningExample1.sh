d8e8cbbb379c0c02c7962f80978701493932e9c4
huangwaleking <huangwaleking@gmail.com>
2017-12-05 10:54:13 -0500

    add running example 1

--- runningExample1.sh
+++ runningExample1.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+if [ ! -d result ]; then
+    mkdir result
+fi
+
+if [ ! -d input ]; then
+    mkdir input
+fi
+
+if [ ! -f input/20newsgroups.txt ]; then
+    wget -c 20newsgroups.txt.zip https://www.dropbox.com/s/2ifzu84j56knvsn/20newsgroups.txt.gz?dl=0 -O input/20newsgroups.txt.gz
+    gzip -d input/20newsgroups.txt.gz
+fi
+
 bash run.sh input/20newsgroups.txt 30 1000 10
