05d3802a3dc9367b1ddfb42167ff641895978fa9
huangwaleking <huangwaleking@gmail.com>
2017-12-05 11:34:02 -0500

    add more information for the running process

--- run.sh
+++ run.sh
@@ -6,6 +6,11 @@ iterationNumber=$3
 numTopPhrases=$4
 
 cd script
+echo ${green}===serialize the input file, to speed up the io of topic modeling===${reset}
 bash runSerializeData.sh ${inputfile}
+
+echo ${green}===build the invert index for phrases===${reset}
 python buildInvertIndex.py ${inputfile}
+
+echo ${green}===topic modeling on phrases===${reset}
 bash runSparseTP.sh ${inputfile} ${topicNumber} ${iterationNumber} ${numTopPhrases}
