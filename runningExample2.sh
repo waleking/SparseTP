d1e96a983cf378faaefd03e6a062eb7762246473
huangwaleking <huangwaleking@gmail.com>
2017-12-05 11:38:59 -0500

    make sure the running example 2 is workable

--- runningExample2.sh
+++ runningExample2.sh
@@ -8,7 +8,7 @@ if [ ! -d input ]; then
     mkdir input
 fi
 
-if [ ! -e input/20newsgroups.txt ]; then
+if [ ! -e input/mathematics.txt ]; then
     wget -c https://www.dropbox.com/s/gbhhe0xogdmwo8x/mathematics.txt.gz?dl=0 -O input/mathematics.txt.gz
     gzip -d input/mathematics.txt.gz
 fi
