e8af8b2e2d9789111e1eafa597c0cbc26de22c67
huangwaleking <huangwaleking@gmail.com>
2017-12-08 12:40:19 -0500

    add running example 3

---
+++ runningExample3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -d result ]; then
+    mkdir result
+fi
+
+if [ ! -d input ]; then
+    mkdir input
+fi
+
+if [ ! -e input/chemistry.txt ]; then
+    wget -c https://www.dropbox.com/s/kv4a0xutepdn8ws/chemistry.txt.gz?dl=0 -O input/chemistry.txt.gz
+    gzip -d input/chemistry.txt.gz
+fi
+
+TopicNumber=50
+IterationNumber=1000
+NumberOfTopPhrasesToShow=10
+
+bash run.sh input/chemistry.txt ${TopicNumber} ${IterationNumber} ${NumberOfTopPhrasesToShow} 
